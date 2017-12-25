5dab006263305c3d04ba1e3ba9f0cee509919265
yTakkar <www.shtakkar@gmail.com>
2017-12-25 22:42:42 +0530

    ..

--- src/components/nothing.js
+++ src/components/nothing.js
@@ -6,7 +6,10 @@ export default class Nothing extends React.Component{
   render(){
     let { mssg, showMssg } = this.props
     return (
-      <div style={nothing} >
+      <div style={{
+        ...nothing,
+        border: !showMssg ? 'none' : null
+      }} >
         <img style={nothing_img} src='../images/large.jpg' />
         { showMssg ? <span style={nothing_span} >{mssg}</span> : null }
       </div>
